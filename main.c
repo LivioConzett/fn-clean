0dc6cc514d18672ac2bd0aa30c379aa4e88d4c94
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2025-05-31 17:35:56 +0200

    got the file seperation working

--- main.c
+++ main.c
@@ -12,37 +12,45 @@
 #include "main.h"
 
 
+static const char ALLOWED_CHARS[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789äöüÄÖÜ_-"};
+static const uint8_t ALLOWED_CHARS_LENGTH = 70;
+
 /**
 * @brief Print out the help menu
 */
 static void print_help(){
     printf("\nfn-clean Help \n");
     printf("-----------------\n");
-    printf("     -h         show this help screen\n");
-    printf("     -t         test. Don't overwrite the filenames. Only print them out.\n");
-    printf("<file chooser>  file whose name to clean\n");
+    printf("     -h  show this help screen\n");
+    printf("     -t  test. Don't overwrite the filenames. Only print them out.\n");
+    printf("<files>  files whose name to clean\n");
     printf("\n");
 }
 
 
 /**
- * @brief get the directory of the filepath
+ * @brief splits up the file path into directory, filename and extension
  * @param length the length of the filepath
  * @param filepath the file path to get the directory from
  * @param directory the directory that is found
  * @param filename filename found
+ * @param extension extension found
  */
-static void get_directory(uint16_t length, char *filepath, char* directory, char* filename){
+static void split_file_path(uint16_t length, char *filepath, char *directory, char *filename, char *extension){
 
     // go through the filepath and count how many '/' there are.
 
     uint8_t slash_amount = 0;
     uint8_t slash_counter = 0;
+    uint8_t dot_amount = 0;
+    uint8_t dot_counter = 0;
     uint16_t filename_counter = 0;
     uint16_t directory_counter = 0;
+    uint16_t extension_counter = 0;
 
     for(uint16_t i = 0; i < length; i++){
         if(filepath[i] == '/') slash_amount++;
+        if(filepath[i] == '.') dot_amount++;
     }
 
     // Go through the filepath. Push everything into the directory buffer
@@ -50,9 +58,15 @@ static void get_directory(uint16_t length, char *filepath, char* directory, char
     // the filename buffer.
     for(uint16_t i = 0; i < length; i++){
 
+        if(filepath[i] == '.') dot_counter++;
+
         if(slash_counter < slash_amount){
             directory[directory_counter] = filepath[i];
-            directory_counter ++;
+            directory_counter++;
+        }
+        else if(dot_amount > 0 && dot_counter >= dot_amount){
+            extension[extension_counter] = filepath[i];
+            extension_counter++;
         }
         else{
             filename[filename_counter] = filepath[i];
@@ -66,12 +80,56 @@ static void get_directory(uint16_t length, char *filepath, char* directory, char
     // if there was no directory, set it to the current one.
     if(directory_counter == 0){
         directory[0] = '.';
-        directory_counter ++;
+        directory[1] = '/';
+        directory_counter = 2;
     }
 
     // end the filename and directory string 
     filename[filename_counter] = 0;
     directory[directory_counter] = 0;
+    extension[extension_counter] = 0;
+
+    // printf("%s - %s - %s\n", directory, filename, extension);
+
+}
+
+/**
+ * @brief checks if a character is safe or not
+ * @param c character to check
+ * @return 1 if c is safe, else 0
+ */
+static uint8_t char_is_safe(char c){
+
+    for(uint8_t i = 0; i < ALLOWED_CHARS_LENGTH; i++){
+        if(ALLOWED_CHARS[i] == c) return 1;
+    }
+
+    return 0;
+}
+
+
+/**
+ * @brief replace the unsafe chars of a string
+ * @param in the filename to change the chars of
+ * @param out the new string with the changed chars
+ */
+static void replace_chars(uint16_t length, char *in, char *out){
+
+    for(uint16_t i = 0; i < length; i++){
+        
+        // If the end was reached, cap off the string and return.
+        if(in[i] == 0){
+            out[i] = 0;
+            return;
+        }
+
+        if(char_is_safe(in[i])){
+            out[i] = in[i];
+        }
+        else{
+            out[i] = '_';
+        }
+    }
 }
 
 
@@ -88,10 +146,12 @@ int main(int argc, char *argv[]){
     // Only testing. Don't overwrite the filenames. Only print them out.
     uint8_t test_param = 0;
     // pointer to the string for the file chooser
-    char *file_chooser = 0;
+    char *file_list[argc];
+
+    uint16_t file_list_counter = 0;
 
     // go through the arguments
-    for(uint8_t i = 1; i < argc; i++){
+    for(uint16_t i = 1; i < argc; i++){
         
         // if the argument starts with a '-' it is a control argument
         if(argv[i][0] == '-'){
@@ -113,49 +173,41 @@ int main(int argc, char *argv[]){
             }
         }
         else{
-            file_chooser = argv[i];
+            file_list[file_list_counter] = argv[i];
+            file_list_counter++;
         }
 
     }
 
     // check if a file chooser was given
-    if(file_chooser == 0){
+    if(file_list_counter == 0){
         fprintf(stderr, "\nNo file chooser was given.\nTry -h for help.\n\n");
         return 0;
     }
 
-    printf("file chooser: %s\n",file_chooser);
+    // go through the files
+    for(int i = 0; i < file_list_counter; i++){
+        // printf("%s\n", file_list[i]);
 
-    uint16_t file_chooser_length = strlen(file_chooser);
+        char* file = file_list[i];
+        uint16_t file_length = strlen(file);
 
-    // get the directory from the file chooser
-    char root_dir[file_chooser_length];
-    char filename[file_chooser_length];
+        // get the directory from the file chooser
+        char root_dir[file_length];
+        char filename[file_length];
+        char extension[file_length];
 
-    get_directory(file_chooser_length, file_chooser, root_dir, filename);
+        split_file_path(file_length, file, root_dir, filename, extension);
 
-    printf("directory: %s\n", root_dir);
-    printf(" filename: %s\n", filename);
+        char new_filename[file_length];
 
-    
-    printf("\n");
+        replace_chars(file_length, filename, new_filename);
 
-    // go through the folder
-    DIR *directory;
-    struct dirent *dir;
+        printf("%s -> %s%s%s\n",file, root_dir, new_filename, extension);
 
-    directory = opendir(root_dir);
+        
 
-    if(directory){
-        while ((dir = readdir(directory)) != NULL){
-            printf("%s\n", dir->d_name);
-        }
-        closedir(directory);
     }
-    else{
-        fprintf(stderr, "\ndirectory %s not found.\n\n", root_dir);
-    }
-
 
     return 0;
 }
